a6a3a91bf3e38f6540d0d706ee86194ef9058942
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-09 19:28:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>jumper Project</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
