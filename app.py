7f955948682c372a82d6416c5b3a2d78e71fc45f
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-09 19:32:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("FlaskFirstProject/home.html")
 
 
 
