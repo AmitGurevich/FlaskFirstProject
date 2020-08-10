0e7a588caba198f3147a361342b9ec85720c64e8
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-10 13:44:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 # A welcome message to test our server
 @app.route("/")
 def home():
-    return render_template("FlaskFirstProject/home.html")
+    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
