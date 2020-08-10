69a26e1abee9cc33a6a5f8c873f7a94b1f47f956
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-10 13:47:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 # A welcome message to test our server
 @app.route("/")
 def home():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
