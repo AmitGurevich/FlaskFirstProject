aa2bff910f7997d2dafe3940d52106d7fec271f0
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-13 23:37:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__),connexion.App(__name__, specification_dir='./')
-
-app.add_api('swagger.yml')
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
-
-@app.route('/peop')
-def people():
-    
-    return render_template('people.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
