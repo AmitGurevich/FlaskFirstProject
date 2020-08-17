6fecaa4dcad26694373e51c180697a07962c434e
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-17 10:58:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,25 +2,15 @@ from flask import Flask, render_template,make_response
 
 import connexion
 
-#app = Flask(__name__)
-
-app = connexion.App(__name__, specification_dir='./')
-
-app.add_api('swagger.yml')
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/peop")
-def peop():
-    return render_template("people.html")
-
 @app.route("/anton")
 def anton():
     return make_response("aaaa", 200)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
