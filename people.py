68a49d296302c6ed65310ec8da06b9363c348832
amit350 <69407198+amit350@users.noreply.github.com>
2020-08-17 10:41:27 +0300

    Update people.py

--- people.py
+++ people.py
@@ -1,4 +1,41 @@
-   # Create the list of people from our data
+  # System modules
+from datetime import datetime
+
+# 3rd party modules
+from flask import make_response, abort
+
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+
+# Data to serve with our API
+PEOPLE = {
+    "Farrell": {
+        "fname": "Doug",
+        "lname": "Farrell",
+        "timestamp": get_timestamp(),
+    },
+    "Brockman": {
+        "fname": "Kent",
+        "lname": "Brockman",
+        "timestamp": get_timestamp(),
+    },
+    "Easter": {
+        "fname": "Bunny",
+        "lname": "Easter",
+        "timestamp": get_timestamp(),
+    },
+}
+
+
+def read_all():
+    """
+    This function responds to a request for /api/people
+    with the complete lists of people
+    :return:        json string of list of people
+    """
+    # Create the list of people from our data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
 
 
